5c10524c5c2bdd34ab1b5c81a7363c2c4f34c678
root <root@ip-172-31-22-166.ec2.internal>
2022-08-03 01:42:08 +0000

    v0.2

--- automation.sh
+++ automation.sh
@@ -35,16 +35,46 @@ fi
 awsc=`dpkg --get-selections awscli | awk '{ print$1 }'`
 if [ "awsc" != "awscli" ]; then
         apt-get install -y awscli
-        
+
 fi
 
 ##CREATING ARCHIVE
 cd /var/log/apache2
 tar cvfz /tmp/$name-httpd-logs-$Date.tar  *.log
+size=`du -sch /tmp/$name-httpd-logs-$Date.tar |head -n 1 | awk '{ print$1 }'`
 
 #Copy the log files to S3 Bucket
 if [ -f "/tmp/$name-httpd-logs-$Date.tar" ]; then
         aws s3 cp  /tmp/$name-httpd-logs-$Date.tar s3://$s3_bucket
-        rm -rfv /tmp/$name-httpd-logs-$Date.tar                       #Remove tar files once copied to S3
 
 fi
+
+#BookKeeping
+
+path=/var/www/html/inventory.html
+
+if [ -f "$path" ]; then
+        echo "Inventory file exists"
+else
+        touch /var/www/html/inventory.html
+        echo -e "\nLog Type\t\t\tTime Created\t\tType\t\tSize" >  /var/www/html/inventory.html
+
+fi
+
+if [ -f "$path" ]; then
+        echo -e "\n$name-httpd-logs\t\t$Date\t\ttar\t\t$size">> /var/www/html/inventory.html
+
+
+fi
+
+cron=/etc/cron.d/automation
+if [ -f "$cron" ]; then
+        echo "Cron Already Set"
+else
+        echo "30 00 * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+        echo "Cron Successfully Set"
+
+fi
+
+#Remove tar file
+rm -rfv /tmp/$name-httpd-logs-$Date.tar
